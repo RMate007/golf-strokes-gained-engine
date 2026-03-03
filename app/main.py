0d0a12de165f8a219775feb7206bea984aa6579a
Ruben <rmatep017@gmail.com>
2026-03-03 14:14:07 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,5 +4,13 @@ from app.engine.strokes_gained import calculate_sg
 app = FastAPI()
 
 @app.get("/calculate")
-def calculate(e_start: float, e_end: float, penalty: int = 0):
-    return {"strokes_gained": calculate_sg(e_start, e_end, penalty)}
+def calculate(
+    start_lie: str,
+    start_distance: float,
+    end_lie: str,
+    end_distance: float,
+    strokes: int = 1,
+    penalty: int = 0,
+):
+    sg = calculate_sg(start_lie, start_distance, end_lie, end_distance, strokes, penalty)
+    return {"strokes_gained": sg}
