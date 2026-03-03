521fae75fcc36606ae30b4eb9f846495ac1923c4
Ruben <rmatep017@gmail.com>
2026-03-03 14:13:53 -0500

    Update strokes_gained.py

--- app/engine/strokes_gained.py
+++ app/engine/strokes_gained.py
@@ -1,2 +1,13 @@
-def calculate_sg(e_start: float, e_end: float, penalty: int = 0) -> float:
-    return e_start - (1 + e_end + penalty)
+from app.engine.baseline_data import get_expected_strokes
+
+def calculate_sg(
+    start_lie: str,
+    start_distance: float,
+    end_lie: str,
+    end_distance: float,
+    strokes: int = 1,
+    penalty: int = 0,
+) -> float:
+    e_start = get_expected_strokes(start_lie, start_distance)
+    e_end = get_expected_strokes(end_lie, end_distance)
+    return round(e_start - (strokes + penalty + e_end), 4)
